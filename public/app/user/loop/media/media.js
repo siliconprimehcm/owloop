5adb7184399cd6ebed0566b7f0fcfbeed03899f2
ThangVo <you@example.com>
2015-06-30 00:56:22 -0400

    loop>> new media 1

--- public/app/user/loop/media/media.js
+++ public/app/user/loop/media/media.js
@@ -1,32 +1,75 @@
 var loopModule = angular.module('owloop.user.loop');
 
 loopModule.controller('mediaController', function ($scope, Restangular, authenticationSvc, Upload, $timeout) {
-    var header = authenticationSvc.getHeader();
-    var param = {
-        "loopId": 860,
-		"lastUpdate": 0,
-		"pageSize": 10
-    };
-
-    Restangular.one('/v1/Loop/GetLoopAlbum').customPOST(param, '', {}, header).then(function (data) {
-    	console.log(data);
-    });
-
-    $scope.createAlbum = function(){
-    	var param = {
+    
+    getAlbum();
+    function getAlbum(){
+    	var header = authenticationSvc.getHeader();
+	    var param = {
 	        "loopId": 860,
 			"lastUpdate": 0,
 			"pageSize": 10
 	    };
+	    Restangular.one('/v1/Loop/GetLoopAlbum').customPOST(param, '', {}, header).then(function (data) {
+	    	console.log(data.objectValue.data);
+	    	$scope.albums = data.objectValue.data;
+	    });
+    }
+    
+
+    $scope.newAlbum = {
+    	title:'',
+    	description:''
+    };
+    $scope.createAlbum = function(){
+    	var header = authenticationSvc.getHeader();
+    	var param = {
+	        "loopId": 860,
+			"title": $scope.newAlbum.title,
+			"description": $scope.newAlbum.description,
+			"photos": $scope.photos
+	    };
 	    
-    	// Restangular.one('/v1/Loop/GetLoopAlbum').customPOST(param, '', {}, header).then(function (data) {
-	    // 	console.log(data);
-	    // });
+    	Restangular.one('/v1/Album/CreateUpdateAlbum').customPOST(param, '', {}, header).then(function (data) {
+	    	console.log(data);
+	    	getAlbum();
+	    	$scope.newAlbum.title = null;
+	    	$scope.newAlbum.description = null;
+	    	$scope.photos = [];
+	    	$(function () {
+			   $('#modalCreateAlbumSecond').modal('toggle');
+			});
+	    });
+	};
+
+	$scope.createAlbumDone = function(){
+		var header = authenticationSvc.getHeader();
+		var param = {
+	        "loopId": 860,
+			"title": $scope.newAlbum.title,
+			"description": $scope.newAlbum.description
+	    };
+	    
+    	Restangular.one('/v1/Album/CreateUpdateAlbum').customPOST(param, '', {}, header).then(function (data) {
+	    	console.log(data);
+	    	getAlbum();
+	    	$(function () {
+			   $('#modalCreateAlbum').modal('toggle');
+			});
+	    });
+	};
+
+	$scope.gotoUpload = function(){
+		if($scope.newAlbum.title === ''){
+			return
+		}
+
 		$(function () {
 		   $('#modalCreateAlbum').modal('toggle');
 		   $('#modalCreateAlbumSecond').modal('toggle');
 		});
 	}
+
 
     $scope.$watch('files', function () {
         $scope.upload($scope.files);
@@ -35,7 +78,9 @@ loopModule.controller('mediaController', function ($scope, Restangular, authenti
 
     $scope.multiple = 1;
 
+    $scope.photos = [];
     $scope.upload = function (files) {
+    	var header = authenticationSvc.getHeader();
     	delete header['Content-type'];
     	console.log(files);
         if (files && files.length) {
@@ -55,10 +100,18 @@ loopModule.controller('mediaController', function ($scope, Restangular, authenti
                                 evt.config.file.name + '\n' + $scope.log;
                 }).success(function (data, status, headers, config) {
                     $timeout(function() {
-                        console.log(data.objectValue.data[0].fileName)
+                        console.log(data.objectValue.data[0].fileName);
+                        data.objectValue.data[0]['description'] = '';
+                        $scope.photos.push(data.objectValue.data[0]);
                     });
                 });
             }
         }
     };
 });
+
+loopModule.filter('trusted', ['$sce', function ($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+}]);
