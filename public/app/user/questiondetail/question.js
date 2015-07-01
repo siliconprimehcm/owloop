2d947584571b2952ed2f9ed3cdb227819e7b163c
ThangVo <you@example.com>
2015-06-30 23:08:23 -0400

    setup question detail

--- public/app/user/questiondetail/question.js
+++ public/app/user/questiondetail/question.js
@@ -1,6 +1,18 @@
 var userModule = angular.module('owloop.user');
 
 userModule.controller('questionController', function ($scope, Restangular, authenticationSvc) {
-    console.log('questionController');
+    
+    getQuestionDetail();
+    function getQuestionDetail(){
+    	var header = authenticationSvc.getHeader();
+	    var param = {
+	        "postId": 860,
+			"pageSize": 10
+	    };
+	    Restangular.one('/v1/Post/GetPostById').customPOST(param, '', {}, header).then(function (data) {
+	    	console.log(data.objectValue.data);
+	    	
+	    });
+    }
    
 });
