e8735bf565b29f781c5a275a4db60b82f5810030
Trong <trongtran@siliconprime.us>
2015-07-01 14:14:30 +0700

    Fix Home

--- public/app/user/homefeed/homefeed.js
+++ public/app/user/homefeed/homefeed.js
@@ -1,16 +1,43 @@
 var userModule = angular.module('owloop.user');
 
-userModule.controller('homefeedController', function($scope, authenticationSvc, Restangular) {
-	console.log('homefeedController');
+userModule.controller('homefeedController', function ($rootScope, $scope, authenticationSvc, Restangular, feedService) {
+    console.log('homefeedController');
+    $rootScope.questionsPosts = [];
+    $rootScope.posts = [];
+    $rootScope.loopPopulars = [];
 
-	var header = authenticationSvc.getHeader();
+    var header = authenticationSvc.getHeader();
     var param = {
         "loopId": 1
-   	};
-	Restangular.one('/v1/Customer/GetHomeFeedInfo').customPOST(param, '', {}, header).then(function (data) {
-		$scope.questionsPosts = data.objectValue.questionsPosts;
-	    $scope.posts = data.objectValue.posts;
-	    $scope.loopPopulars = data.objectValue.loopPopulars;
-	})
+    };
+
+    $scope.loadMoreFeed = function () {
+        var promise = feedService.getFeed(header, param);
+        promise.then(function (result) {
+            for (var i = 0; i < result.questionsPosts.length; i++) {
+                $rootScope.questionsPosts.push(result.questionsPosts[i]);
+            }
+            for (var i = 0; i < result.posts.length; i++) {
+                $rootScope.posts.push(result.posts[i]);
+            }
+        });
+    };
 });
+userModule.factory('feedService', function ($q, Restangular) {
+    return {
+        getFeed: function (header, param) {
+            var deferred = $q.defer();
+
+            Restangular.one('/v1/Customer/GetHomeFeedInfo').customPOST(param, '', {}, header).then(function (data) {
+                deferred.resolve({
+                    questionsPosts: data.objectValue.questionsPosts,
+                    posts: data.objectValue.posts,
+                    loopPopulars: data.objectValue.loopPopulars
+                });
+            });
+
+            return deferred.promise;
+        }
+    }
+})
 
